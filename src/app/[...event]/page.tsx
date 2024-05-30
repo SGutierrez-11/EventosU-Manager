ce6e1b6d6fa24880aa494df4614fd0c5ce3ee81a
Santiago Gutiérrez Villegas <1006426560@u.icesi.edu.co>
2024-05-30 08:42:09 -0500

    Add comments in details

--- src/app/[...event]/page.tsx
+++ src/app/[...event]/page.tsx
@@ -1,66 +1,14 @@
-"use client"
+"use client";
 import TypeORMEvent from "@/api/infrastructure/data-sources/typeorm/models/mongo/Event";
 import React, { use, useEffect, useState } from "react";
-
-const eventData = {
-  title: "Conferencia Internacional de Tecnología",
-  description:
-    "Un evento para explorar las últimas tendencias en tecnología e innovación.",
-  categories: ["Tecnología", "Innovación", "Desarrollo"],
-  date: "2024-08-15",
-  location: {
-    name: "Centro de Convenciones Internacional",
-    address: "123 Calle Principal",
-    city: { name: "Bogotá", department: "Bogotá", country: "Colombia" },
-  },
-  attendees: [
-    {
-      username: "user01",
-      fullName: "Juan Pérez",
-      relationshipType: "Professional",
-      email: "juan@example.com",
-      city: {
-        name: "Cali",
-        department: "Valle del Cauca",
-        country: "Colombia",
-      },
-    },
-    {
-      username: "user02",
-      fullName: "Laura Martínez",
-      relationshipType: "Academic",
-      email: "laura@example.com",
-      city: { name: "Medellín", department: "Antioquia", country: "Colombia" },
-    },
-  ],
-  speakers: [
-    {
-      username: "speaker01",
-      fullName: "Carlos Gómez",
-      relationshipType: "Speaker",
-      email: "carlos@example.com",
-      city: { name: "Bogotá", department: "Bogotá", country: "Colombia" },
-    },
-  ],
-  organizingFaculties: ["Facultad de Ingeniería", "Facultad de Ciencias"],
-  organizingProgram: "Programa de Ingeniería de Sistemas",
-  comments: [
-    {
-      text: "¡Estoy muy emocionado por este evento!",
-      user: {
-        fullName: "Ana López",
-        username: "ana01",
-        email: "ana@example.com",
-      },
-    },
-  ],
-};
-
 
 const url = "http://localhost:3000/api";
 
 const EventProfilePage = ({ params }: { params: { id: string } }) => {
   const [event, setEvent] = useState<TypeORMEvent>({} as TypeORMEvent);
+  const [newComment, setNewComment] = useState("");
+  const [commenterId, setCommenterId] = useState("");
+
   useEffect(() => {
     fetch(`${url}/api/event/${params.id}`).then((res) =>
       res.json().then((data) => {
@@ -68,15 +16,48 @@ const EventProfilePage = ({ params }: { params: { id: string } }) => {
       })
     );
   }, []);
+
+  const handleAddComment = async () => {
+    const updatedEvent = {
+      ...event,
+      comments: [
+        ...event.comments,
+        { text: newComment, user: { id: commenterId } },
+      ],
+    };
+
+    const response = await fetch(`${url}/event/${params.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedEvent),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setEvent(data.Event); // Actualiza el evento con la nueva lista de comentarios
+      setNewComment("");
+      setCommenterId("");
+    }
+  };
+
   return (
     <div className="max-w-4xl m-auto bg-white shadow-lg rounded-lg">
       <div className="bg-orange-500 p-4 rounded-t-lg">
-        <h1 className="text-4xl font-bold text-white text-center">{event.title}</h1>
+        <h1 className="text-4xl font-bold text-white text-center">
+          {event.title}
+        </h1>
       </div>
       <div className="p-12">
         <p className="text-xl font-bold mb-12">{event.description}</p>
         <p className="font-semibold mb-2">
-          Categorías: {event.categories.join(", ")}
+          Categorías:{" "}
+          {event.categories == undefined ? (
+            <p>No hay categorias</p>
+          ) : (
+            event.categories.join(", ")
+          )}
         </p>
         <p className="font-semibold mb-2">Fecha: {event.date}</p>
         <p className="font-semibold mb-2">
@@ -104,12 +85,37 @@ const EventProfilePage = ({ params }: { params: { id: string } }) => {
           </p>
         ))}
 
-        <h2 className="text-3xl font-extrabold mt-6 mb-2">Comentarios</h2>
-        {event.comments.map((comment) => (
-          <p key={comment.text} className="ml-4 mb-2">
-            {comment.text} - {comment.user.fullName}
-          </p>
-        ))}
+        <div className="p-12">
+          <textarea
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Escribe un comentario..."
+            className="w-full p-2 border"
+          ></textarea>
+          <select
+            value={commenterId}
+            onChange={(e) => setCommenterId(e.target.value)}
+            className="w-full mt-2 p-2 border"
+          >
+            <option value="">Seleccione un usuario</option>
+            {event.attendees.map((attendee) => (
+              <option key={attendee.id} value={attendee.id}>
+                {attendee.fullName}
+              </option>
+            ))}
+          </select>
+          <button
+            onClick={handleAddComment}
+            className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Añadir Comentario
+          </button>
+          {event.comments.map((comment, index) => (
+            <p key={index} className="mt-2">
+              {comment.text} - {comment.user.fullName}
+            </p>
+          ))}
+        </div>
       </div>
     </div>
   );
